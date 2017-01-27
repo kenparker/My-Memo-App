5152f63649922dd147fe3ab7c083db708e6ad4c6
angelo <magangde@yahoo.de>
2017-01-27 16:27:28 +0100

    UsingGetToReadFormData Demo enhanced

--- src/main/java/com/maggioni/basic/UsingGetToReadFormData.java
+++ src/main/java/com/maggioni/basic/UsingGetToReadFormData.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = {"getToRead1","getToRead2"})
+@WebServlet(urlPatterns = {"UsingGetToReadFormData","UsingGetToReadFormData2"})
 public class UsingGetToReadFormData extends HttpServlet {
 
     @Override
