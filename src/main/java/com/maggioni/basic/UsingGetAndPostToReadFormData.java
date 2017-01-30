5e415f24e7b7b9792e153c53ea340e3fbdd4ce09
magang <magang@magang-PC.fritz.box>
2017-01-30 12:21:54 +0100

    Binding POST to Servlet

--- src/main/java/com/maggioni/basic/UsingGetAndPostToReadFormData.java
+++ src/main/java/com/maggioni/basic/UsingGetAndPostToReadFormData.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = {"UsingGetToReadFormData", "UsingGetToReadFormData2"})
+@WebServlet(urlPatterns = {"UsingGetToReadFormData", "UsingPostToReadFormData"})
 public class UsingGetAndPostToReadFormData extends HttpServlet {
 
     @Override
