7ea83f88a277ae78bc0edb2f6002c73ab2e717d7
magang <magang@magang-PC.fritz.box>
2017-01-30 12:11:40 +0100

    Refactoring charset=utf-8 and Reading Form Data using POST

--- src/main/java/com/maggioni/basic/HelloWorldWithInit.java
+++ src/main/java/com/maggioni/basic/HelloWorldWithInit.java
@@ -22,6 +22,7 @@ public class HelloWorldWithInit extends HttpServlet{
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
+        resp.setCharacterEncoding("utf-8");
         
         PrintWriter out = resp.getWriter();
         out.print("<h1>" +  message + "</h1>");
---
+++ src/main/java/com/maggioni/basic/UsingGetAndPostToReadFormData.java
@@ -0,0 +1,42 @@
+package com.maggioni.basic;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = {"UsingGetToReadFormData", "UsingGetToReadFormData2"})
+public class UsingGetAndPostToReadFormData extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+        String title = "Using GET Method to Read Form Data";
+        String docType = "<!doctype html public \"-//w3c//dtd html 4.0 "
+                + "transitional//en\">\n";
+
+        String meta = "<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>";
+
+        out.println(docType + meta + "<html>\n"
+                + "<head><title>" + title + "</title></head>\n"
+                + "<body bgcolor=\"#f0f0f0\">\n"
+                + "<h1 align=\"center\">" + title + "</h1>\n"
+                + "<ul>\n"
+                + "  <li><b>First Name</b>: "
+                + request.getParameter("first_name") + "\n"
+                + "  <li><b>Last Name</b>: "
+                + request.getParameter("last_name") + "\n"
+                + "</ul>\n"
+                + "</body></html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
