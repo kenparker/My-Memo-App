09cccb74139c80aea6819afd51874e01576f3473
angelo <magangde@yahoo.de>
2017-02-03 10:07:45 +0100

    minor bug fixed (Servlets uri) and refactoring

--- src/main/java/com/maggioni/basic/CheckBoxServlet.java
+++ src/main/java/com/maggioni/basic/CheckBoxServlet.java
@@ -3,10 +3,12 @@ package com.maggioni.basic;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet(urlPatterns = {"CheckBoxServlet"})
 public class CheckBoxServlet extends HttpServlet {
     private String parameterChemistry;
     private String parameterPhysics;
